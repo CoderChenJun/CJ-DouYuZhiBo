cd57790c101a3b759e125754c4961f1f51f0d657
apple <Jingjing@appledeimac.local>
2016-11-04 09:19:04 +0800

    初始化项目

---
+++ CJ-DouYuZhiBo/CJ-DouYuZhiBo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CJ-DouYuZhiBo
+//
+//  Created by Jingjing Huang on 16/11/4.
+//  Copyright © 2016年 ChenJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CJ-DouYuZhiBo/CJ-DouYuZhiBo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CJ-DouYuZhiBo
+//
+//  Created by Jingjing Huang on 16/11/4.
+//  Copyright © 2016年 ChenJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
